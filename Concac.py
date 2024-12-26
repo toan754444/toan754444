89695baaa264714abd577f67ec6fb6358c97e191
toan754444 <khongtruong2kar8@gmail.com>
2024-12-26 22:00:06 +0700

    Create Concac.py

---
+++ Concac.py
@@ -0,0 +1,6 @@
+def main():
+    print("Xin chào! Chúc bạn một ngày tốt lành!,liên hệ zalo 0928175645,Facebook:https://www.facebook.com/khongtruongelainaa?mibextid=ZbWKwL")
+
+if __name__ == "__main__":
+    main()
+
