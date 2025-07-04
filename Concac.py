238bef6145a98bca196993bf2a58e6b020fb2dbc
toan754444 <khongtruong2kar8@gmail.com>
2025-07-04 21:31:32 +0700

    Delete Concac.py

--- Concac.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Xin chào! Chúc bạn một ngày tốt lành!,liên hệ zalo 0928175645,Facebook:https://www.facebook.com/khongtruongelainaa?mibextid=ZbWKwL")
-
-if __name__ == "__main__":
-    main()
-
